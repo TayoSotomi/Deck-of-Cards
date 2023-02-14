df257d50d773f8ecee49c9765d7f425534237b54
Tee <sotomi.tayo@gmail.com>
2023-02-14 00:15:22 -0600

    First submit

---
+++ Deck Of Cards/Models/DeckDAL.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Deck_Of_Cards.Models
+{
+    public class DeckDAL
+    {
+        public static DeckModel GetDeckModel()
+        {
+            //Adjust here
+            //setup
+            string url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //save the response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //convert to C#
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return result;
+        }
+    }
+}
